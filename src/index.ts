4ea0460ee5498f12ed20cd3f73cfe7430c43e503
Pulasthi Narada <pulasthinarada@hotmail.com>
2022-08-20 22:47:51 +0530

    all commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log('ff');
+const toDoLists: string[] = [
+  'Complete typeScript Course 🚀',
+  'Complete python Course 🔥',
+  'watch modern family season 9 😆 ',
+];
+
+// add to do in  toDoLists wen page load
+const addToDo = (): void => {
+  for (let toDo of toDoLists) {
+    $('ul  ').append(
+      `<li> <span><i class='fa fa-trash'></i></span> 
+      ${toDo}
+      </li>`,
+    );
+  }
+};
+
+// add new to do
+const addNewToDo = (): void => {
+  $("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+      //grabbing new todo text from input
+      var todoText = $(this).val();
+      $(this).val('');
+      //create a new li and add to ul
+      $('ul').append(
+        "<li><span><i class='fa fa-trash'></i></span> " + todoText + '</li>',
+      );
+    }
+  });
+};
+
+//  delete toDo
+const deleteToDo = (): void => {
+  //Click on X to delete Todo
+  $('ul').on('click', 'span', function (event) {
+    $(this)
+      .parent()
+      .fadeOut(500, function () {
+        $(this).remove();
+      });
+    event.stopPropagation();
+  });
+};
+
+addToDo();
+addNewToDo();
+deleteToDo();
+
+$('.fa-plus').click(function () {
+  $("input[type='text']").fadeToggle();
+});
+
+// Check Off Specific Todos By Clicking
+$('ul').on('click', 'li', function () {
+  $(this).toggleClass('completed');
+});
